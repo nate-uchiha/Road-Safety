bf25a27211218607bea7244fe9313b8b3c723493
nate-uchiha <shubham22497@gmail.com>
2019-09-10 22:49:14 +0530

    Create User through Rest API
    added rest framework api to create user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from users.serializer import UserProfileSerializer
+from users.models import UserProfile
+
 
 # Create your views here.
+class UserProfileCreateAPIView(generics.CreateAPIView):
+    query_set = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (AllowAny,)
